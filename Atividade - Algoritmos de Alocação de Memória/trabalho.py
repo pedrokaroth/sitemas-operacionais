c0e6a3ceb5a8f71fe2492b6bb94dada5f43c34b1
pedrokaroth <56929916+pedrokaroth@users.noreply.github.com>
2020-09-03 15:21:27 -0300

    Atividade - Algoritmos de Alocação de Memória
    Desenvolva os algoritmos de alocação de partições em memória. Pode ser utilizado qualquer linguagem de programação.  Trabalho em equipe de até 4 pessoas. Favor informar equipes no fórum - Equipes - Atividade - Algoritmos de Alocação de Memória.
    
    a) O programa deve pergunta ao usuário qual algoritmo será utilizado. As opções são best-fit, worst-fit e first-fit.
    
    b) O programa irá considerar uma memória de tamanho fixo de 20 unidades. A memória irá iniciar com algumas posições alocadas representadas pela letra X. Por exemplo: [ X, -, -, -, -, X, -, -, -, -, X, X, -, -, -, -, -, X, -, - ].  A configuração inicial da memória deverá ser exibida em tela.
    
    c) O programa deverá solicitar ao usuário o tamanho em unidades do programa que deseja alocar. O programa deverá alocar o programa solicitado e exibir em tela a configuração atual da memória. O número representa o programa alocado. Por exemplo: [ X, 1, 1 , -, -, X, -, -, -, -, X, X, -, -, -, -, -, X, -, - ]. Caso não for possível alocar, o programa deverá avisar.
    
     
    
    d) Bônus: O programa deverá desalocar da memória o programa informado pelo usuário. Cada programa alocado pode ser associado a um número. O programa deverá exibir em tela a configuração atual da memória.
    
     
    
    e) Grave um vídeo (youtube/MS Teams) explicando os algoritmos. Duração máxima de 5 minutos.
    
     
    
    Entrega da Atividade
    
     
    
    Adicione os códigos no repositório do GitHub. Coloque em um diretório separado. 
    
    Não deve colocar arquivo zipado no github.
    
    Na resposta da ferramenta, deverá ser informado o link do github e o link para o vídeo.
    
    Será disponibilizado duas aulas (25/08 e 01/09) para o desenvolvimento do trabalho. O prazo não será adiado.
    
     
    
    Critérios de avaliação
    
    Resposta no prazo previsto
    Percentual de acerto

---
+++ Atividade - Algoritmos de Alocação de Memória/trabalho.py
@@ -0,0 +1,92 @@
+from random import randint
+from os import system
+menuOption = 0
+
+# Opa professor vou colocar alguns comentarios para explicar a logica mas gostaria que voce avaliasse o codigo pela legibilidade, abrc!
+
+def setMemory():
+	memory = [' '] * 100
+
+	for x in range(len(memory)):
+
+		# 50% de chance de cair X ou vazio
+		if(randint(1,5) == 1):
+			memory[x] = 'X'
+		else:
+			memory[x] = ' '
+
+	return memory
+
+def showMemory(memory):
+	print('-' * 14 + 'MEMORIA ATUAL' + '-' * 13)
+
+	# Loop que aumenta de 20 em 20 para mostrar a memoria na tela
+	for x in range(0,5):
+		if x == 0:
+			startPrint = 0
+			endPrint = 20
+		else:
+			startPrint = endPrint
+			endPrint += 20
+
+		for i in range(startPrint, endPrint):
+			print(memory[i], end = "|")
+		print()
+
+
+memory = setMemory()
+showMemory(memory)
+
+menu = int()
+prog = 0
+
+while menu != 5:
+	print("+--------------------------------------+")
+	print("|          ALOCAÇÃO DE MEMÓRIA         |")
+	print("+------+-------------------------------+")
+	print("|  1   |          BEST-FIT             |")
+	print("|  2   |          WORST-FIT            |")
+	print("|  3   |          FIRST-FIT            |")
+	print("|  4   |          REMOVER              |")
+	print("|  5   |          SAIR                 |")
+	print("+--------------------------------------+")
+
+	if(menu != 5): 
+
+		menu = int(input())
+		alocated = False
+		
+		if menu == 3: 		
+			size = int(input("Tamanho do programa: "))
+			free = 0
+
+
+			for x in range(len(memory)):
+
+				if free == size:
+					
+					prog += 1
+					for i in range(x-size, x):
+						memory[i] = prog
+					alocated = True
+					break
+
+				if memory[x] == " ":
+					free += 1
+
+				else:
+					free = 0
+					
+			if not alocated:
+				print("Memória insuficiente =(")
+
+		elif menu == 4:
+			remove = int(input("Programa a remover: "))
+
+			for x in range(len(memory)):
+				if memory[x] == remove:
+					memory[x] = " "
+
+		showMemory(memory)
+
+		
